8d1b4c48be42af5ff7eb4d574edc962b2f38910e
Leandro <lsoares199658@gmail.com>
2021-03-13 02:27:22 -0300

    'fix'

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import ReactGA from "react-ga";
 function App() {
   const TRACKING_ID = "G-KDWJTZRBTR"; // YOUR_OWN_TRACKING_ID
   ReactGA.initialize(TRACKING_ID);
+  ReactGA.pageview(window.location.pathname + window.location.search);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload..
+          Edit <code>src/App.js</code> and save to reloadd..
         </p>
         <a
           className="App-link"
